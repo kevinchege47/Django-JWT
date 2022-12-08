0be4c56e5170e34482567caba69bd1654ae8bc93
kevinchege47 <chegekevin001@gmail.com>
2022-12-08 22:49:58 +0300

    Login and logout working successfully with Jwt Cookies

--- Jwt/settings.py
+++ Jwt/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'application.User'
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Registerview,Loginview
+from .views import Registerview,Loginview,Userview,Logoutview
 
 urlpatterns = [
     path('register',Registerview.as_view()),
-    path('login',Loginview.as_view())
+    path('login',Loginview.as_view()),
+    path('user',Userview.as_view()),
+    path('logout',Logoutview.as_view()),
 ]
