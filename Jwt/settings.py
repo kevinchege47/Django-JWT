721927a2178ea48bb0ed28b9f52470c9cac0166c
kevinchege47 <chegekevin001@gmail.com>
2022-12-08 17:16:03 +0300

    Added Postgres Configurations

--- Jwt/settings.py
+++ Jwt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y1cf4!+4y-iyrd#^_%c3sxns5c2t9edaxook53=@nodg@op5cd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'application'
+    'application',
+    'rest_framework',
+  
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,21 @@ WSGI_APPLICATION = 'Jwt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'JWT',
+        'PASSWORD':'71796555',
+        'PORT':'5432',
+        'HOST':'127.0.0.1',
+        'USER' : 'postgres'
+
     }
 }
 
@@ -117,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'application.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
